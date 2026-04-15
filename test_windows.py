12500921d41cd808780051300854be4889eea366
LIU Hui <atliuhui@outlook.com>
2026-04-15 16:54:53 +0800

    add Windows tool

---
+++ test_windows.py
@@ -0,0 +1,9 @@
+from tools.Windows import Tools
+
+tools = Tools()
+
+result = tools.archive_desktop_images()
+print(result)
+
+result = tools.archive_screen_images()
+print(result)
---
+++ tools/Windows.py
@@ -0,0 +1,103 @@
+"""
+title: Windows
+description: Windows系统相关工具集合
+version: 0.1.0
+author: Hui LIU
+license: MIT
+requirements:
+required_open_webui_version: 0.8.12
+"""
+
+import os
+import shutil
+from pathlib import Path
+from datetime import datetime
+
+
+class Tools:
+    def __init__(self):
+        pass
+
+    # 包括保存桌面图片、保存屏保图片等功能
+
+    def archive_desktop_images(
+        self,
+    ) -> str:
+        """
+        保存桌面图片
+        """
+
+        user = os.getenv("USERPROFILE")
+        source_path = rf"{user}\AppData\Roaming\Microsoft\Windows\Themes"
+        target_path = rf"{user}\Downloads\screen-assets"
+
+        def file_filter(file):
+            return file.suffix.lower() not in {
+                ".ini",
+            }
+
+        def file_rename(file):
+            time = file.stat().st_mtime
+            name = datetime.fromtimestamp(time).strftime("%Y%m%d")
+            return f"{name}.jpg"
+
+        return self._archive_images(
+            source_path,
+            target_path,
+            file_rename,
+            file_filter,
+        )
+
+    def archive_screen_images(
+        self,
+    ) -> str:
+        """
+        保存屏保图片
+        """
+
+        user = os.getenv("USERPROFILE")
+        source_path = (
+            rf"{user}\AppData\Local\Packages"
+            r"\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy"
+            r"\LocalState\Assets"
+        )
+        target_path = rf"{user}\Downloads\screen-assets"
+
+        def file_rename(file):
+            return f"{file.name}.jpg"
+
+        return self._archive_images(
+            source_path,
+            target_path,
+            file_rename,
+        )
+
+    def _archive_images(
+        self,
+        source_path,
+        target_path,
+        file_rename,
+        file_filter=None,
+    ):
+        """
+        保存图片通用逻辑
+        """
+        try:
+            source = Path(source_path)
+            target = Path(target_path)
+            target.mkdir(parents=True, exist_ok=True)
+
+            for file in source.rglob("*"):
+                if not file.is_file():
+                    continue
+                if file_filter and not file_filter(file):
+                    continue
+
+                dest = target / file_rename(file)
+                shutil.copy2(file, dest)
+
+            return f"保存已经完成，保存的路径是{target_path}"
+        except Exception as e:
+            print(e)
+
+            return str(e)
